6f366b30076cfb9f8f82664b720fcb2ed3ccd15a
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-01-04 16:27:38 +0000

    initial file setup

---
+++ src/js/components/views/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Formsy from 'formsy-react';
+import { Input } from 'formsy-react-components';
+
+let Home = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+});
+
+module.exports = Home;
