e9c039b4e9e25f6b478254007b4f48eeef11cb81
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-01-06 22:20:26 +0000

    moving Hava to the far right of navbar

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 var NavBar = React.createClass({
   render: function() {
       return (
-        <div className="navbar">
+        <div className="navbar pull-right">
           <h4>HAVA</h4>
         </div>
       );
