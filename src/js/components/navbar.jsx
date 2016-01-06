0a511d785f22500733a6fbe12b653995c1f84c34
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-01-06 22:42:21 +0000

    moving hava to the right

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 var NavBar = React.createClass({
   render: function() {
       return (
-        <div className="navbar pull-right">
+        <div className="navibar pull-right">
           <h4>HAVA</h4>
         </div>
       );
