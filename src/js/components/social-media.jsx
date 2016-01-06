1183af41eaddfac60727be3b5806aad8ba2b415f
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-01-06 10:55:25 +0000

    updating state instantly  with a callback

--- src/js/components/social-media.jsx
+++ src/js/components/social-media.jsx
@@ -8,7 +8,7 @@ let SocialMediaLinks = React.createClass({
           <img className='icon-in' src="https://cdn1.iconfinder.com/data/icons/simple-icons/4096/instagram-4096-black.png"/>
         </a>
         <a href="https://www.facebook.com/">
-          <img className='icon-fb' src="http://vectorlogofree.com/wp-content/uploads/2014/07/25305-facebook-sign-of-letter-f-inside-a-black-rounded-square-shape-icon-vector-icon-vector-eps.png"/>
+          <img className='icon-fb' src="https://vectorlogofree.com/wp-content/uploads/2014/07/25305-facebook-sign-of-letter-f-inside-a-black-rounded-square-shape-icon-vector-icon-vector-eps.png"/>
         </a>
         <a href="https://twitter.com/">
           <img className='icon-tw' src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/twitter_circle_black-512.png"/>
