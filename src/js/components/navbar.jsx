b68a3c91f5029edce189edf71aecad6b4f321bf6
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-01-10 20:13:22 +0000

    adding logo to landing page

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -4,7 +4,7 @@ var NavBar = React.createClass({
   render: function() {
       return (
         <div className="navibar pull-right">
-          <h3>HAVA</h3>
+          <img src="https://cloud.githubusercontent.com/assets/12450298/12223536/2b717360-b7d2-11e5-873c-9d82119af5c4.png"/>
         </div>
       );
   }
