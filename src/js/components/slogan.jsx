0d8c018a44a57040f8dffc301c9881b81f7e229e
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-02-18 20:35:31 +0000

    changing the text

--- src/js/components/slogan.jsx
+++ src/js/components/slogan.jsx
@@ -12,7 +12,7 @@ let Slogan = React.createClass({
           <h2>Welcome to the Hava App!</h2>
         </div>
         <div className='slogan-text'>
-          <p>We know it's cold but it's time to stop being a hermit! Join us in March in SW London when we give you the perfect reason to hit one of your best local bars!</p>
+          <p>Join us in March in SW London when we give you the perfect reason to hit one of your best local bars!</p>
         </div>
       </div>
     );
