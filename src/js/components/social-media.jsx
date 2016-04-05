cddbe7924e2cf579a45e7d5e3c16c28b190bbb06
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-04-05 08:53:45 +0100

    changes the opacity of the background image

--- src/js/components/social-media.jsx
+++ src/js/components/social-media.jsx
@@ -11,13 +11,13 @@ let SocialMediaLinks = React.createClass({
         </div>
         <div className='row social'>
           <a href="https://www.instagram.com/thehavaapp/">
-            <img className='icon-in' src="https://cdn1.iconfinder.com/data/icons/simple-icons/4096/instagram-4096-black.png"/>
+            <img className='icon-in' src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697067-instagram-512.png"/>
           </a>
           <a href="https://www.facebook.com/havaapp/">
-            <img className='icon-fb' src="https://vectorlogofree.com/wp-content/uploads/2014/07/25305-facebook-sign-of-letter-f-inside-a-black-rounded-square-shape-icon-vector-icon-vector-eps.png"/>
+            <img className='icon-fb' src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697057-facebook-512.png"/>
           </a>
           <a href="https://twitter.com/thehavaapp">
-            <img className='icon-tw' src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/twitter_circle_black-512.png"/>
+            <img className='icon-tw' src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697029-twitter-512.png"/>
           </a>
         </div>
       </div>
